d8705a743525acc8123de8b37951e4ed9620d640
CDLemmi <c.lemmerich@gmx.de>
2024-08-01 20:49:58 +0200

    first commit

---
+++ app/src/main/java/de/cdlemmi/bbe/App.java
@@ -0,0 +1,97 @@
+package de.cdlemmi.bbe;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryStack;
+
+public class App {
+    private long window;
+
+    public void run() {
+        init();
+        loop();
+
+        // Free the window callbacks and destroy the window
+        Callbacks.glfwFreeCallbacks(window);
+        GLFW.glfwDestroyWindow(window);
+
+        // Terminate GLFW and free the error callback
+        GLFW.glfwTerminate();
+        GLFW.glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        // Set up an error callback. The default implementation will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Configure GLFW
+        GLFW.glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE); // the window will stay hidden after creation
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        window = GLFW.glfwCreateWindow(800, 600, "Hello LWJGL", 0, 0);
+        if (window == 0) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        // Get the thread stack and push a new frame
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            //org.lwjgl.system.MemoryUtil.memStackPush();
+
+            // Center the window
+            org.lwjgl.glfw.GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - 800) / 2,
+                    (vidmode.height() - 600) / 2
+            );
+
+            // Make the OpenGL context current
+            GLFW.glfwMakeContextCurrent(window);
+
+            // Enable v-sync
+            GLFW.glfwSwapInterval(1);
+
+            // Make the window visible
+            GLFW.glfwShowWindow(window);
+        }
+    }
+
+    private void loop() {
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+        // Set the clear color
+        GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        // Run the rendering loop until the user attempts to close the window
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            GLFW.glfwSwapBuffers(window); // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            GLFW.glfwPollEvents();
+        }
+    }
+
+    public static void main(String[] args) {
+		System.out.println("Hello World");
+        new App().run();
+		System.out.println("program finished");
+    }
+}
